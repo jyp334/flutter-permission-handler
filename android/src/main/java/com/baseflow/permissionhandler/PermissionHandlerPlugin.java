9c35bb069ce93c29eb6157ec5c79853b53b8cd23
jyp334 <jiyupeng@aragoncs.com>
2020-11-06 10:52:18 +0800

    change android support to androidx

--- android/src/main/java/com/baseflow/permissionhandler/PermissionHandlerPlugin.java
+++ android/src/main/java/com/baseflow/permissionhandler/PermissionHandlerPlugin.java
@@ -16,12 +16,12 @@ import android.os.Build.VERSION_CODES;
 import android.os.PowerManager;
 import android.provider.Settings;
 
-import android.support.annotation.IntDef;
-import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.NotificationManagerCompat;
-import android.support.v4.content.ContextCompat;
+import androidx.annotation.IntDef;
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
 import android.telephony.TelephonyManager;
 import android.text.TextUtils;
 import android.util.Log;
